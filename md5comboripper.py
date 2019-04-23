686a44651b427edc2cc9455e9b3d7f0e9a557a53
luk0y <45989946+luk0y@users.noreply.github.com>
2019-04-23 08:31:38 +0530

    Update md5comboripper.py

--- md5comboripper.py
+++ md5comboripper.py
@@ -1,30 +1,30 @@
 import hashlib
 
 
-count=0
-count2=0
+found_hash=0
+hash_count=0
 combos=input("\nEnter the combos file name where the passwords are md5 encrypted : ")
 file1=input("\nEnter the wordlist : ")
 decrypted=open("decrypted.txt", "a+") 
 print("\n")
 with open(combos) as data:
     for encline in data:
-        for ch in encline:
-            if(ch==":"):
-                str1= encline[encline.index(ch)+1:-1]
+        for character in encline:
+            if(character==":"):
+                str1= encline[encline.index(character)+1:-1]
                 with open(file1) as data:
                     for line in data:
                         line=line[:-1]
                         str2 = hashlib.md5(line.encode())
                         str2 = str2.hexdigest()
                         if(str1==str2):
-                             decrypted.write(encline[0:encline.index(ch)]+":"+line+"\n")# Decrypted ones can be saved in decrypted.txt file
-                             count=count+1
-                             print(count,"found")
+                             decrypted.write(encline[0:encline.index(character)]+":"+line+"\n")# Decrypted ones can be saved in decrypted.txt file
+                             found_hash=found_hash+1
+                             print(found_hash,"found")
                              break
-        count2=count2+1
-        if(count2%50==0):
-            print("\n------------------------",count2,"hashes checked till you------------------------\n")
+        hash_count=hash_count+1
+        if(hash_count%50==0):
+            print("\n------------------------",hash_count,"hashes checked till you------------------------\n")
 data.close()
 combos.close()
 file1.close()
